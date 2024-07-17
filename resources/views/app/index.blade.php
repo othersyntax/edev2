88928ad7ed0e7a8aa8def49927a2bc52324295fe
othersyntax <othersyntax@gmail.com>
2024-07-17 14:54:58 +0800

    Dashboard baru

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -14,50 +14,50 @@
 
     <div class="col-lg-3">
         <div class="ibox ">
-            <div class="ibox-title">
-                <h5>Belanja</h5>
+            <div class="ibox-title bg-success">
+                <h5>BELANJA</h5>
             </div>
             <div class="ibox-content">
-                <h1 class="no-margins">@duit(275800)</h1>
-                <div class="stat-percent font-bold text-info">20% <i class="fa fa-level-up"></i></div>
-                <small>New orders</small>
+                <h1 class="no-margins text-right">@duit(275800)</h1>
+                <div class="stat-percent font-bold text-info">20%</div>
+                <small>Prestasi</small>
             </div>
         </div>
     </div>
     <div class="col-lg-3">
         <div class="ibox ">
-            <div class="ibox-title">
-                <h5>Tanggungan</h5>
+            <div class="ibox-title bg-warning">
+                <h5>TANGGUNGAN</h5>
             </div>
             <div class="ibox-content">
-                <h1 class="no-margins">@duit(106120)</h1>
-                <div class="stat-percent font-bold text-navy">44% <i class="fa fa-level-up"></i></div>
-                <small>New visits</small>
+                <h1 class="no-margins text-right">@duit(106120)</h1>
+                <div class="stat-percent font-bold text-navy">30% <i class="fa fa-level-up"></i></div>
+                <small>Peratusan</small>
             </div>
         </div>
     </div>
     <div class="col-lg-3">
         <div class="ibox ">
-            <div class="ibox-title">
-                <h5>Penjimatan</h5>
+            <div class="ibox-title bg-info">
+                <h5>PENJIMATAN</h5>
             </div>
             <div class="ibox-content">
-                <h1 class="no-margins">@duit(86200)</h1>
-                <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
-                <small>Total income</small>
+                <h1 class="no-margins text-right">@duit(86200)</h1>
+                <div class="stat-percent font-bold text-success">5% <i class="fa fa-bolt"></i></div>
+                <small>Peratusan</small>
             </div>
         </div>
     </div>
     <div class="col-lg-3">
         <div class="ibox ">
-            <div class="ibox-title">
+            <div class="ibox-title bg-primary">
                 <span class="label label-success float-right">{{ date('Y') }}</span>
-                <h5>JUMLAH</h5>
+                <h5>SILING</h5>
             </div>
             <div class="ibox-content">
-                <h1 class="no-margins">@duit(40886200)</h1>
-                <div class="stat-percent font-bold text-danger">38% <i class="fa fa-level-down"></i></div>
-                <small>In first month</small>
+                <h1 class="no-margins text-right text-info"><b>@duit(40886200)</b></h1>
+                <div class="stat-percent font-bold text-danger">20%</i></div>
+                <small>Prestasi</small>
             </div>
         </div>
     </div>
@@ -68,23 +68,16 @@
     <div class="col-lg-12">
         <div class="ibox ">
             <div class="ibox-title">
-                <h5>Orders</h5>
-                <div class="float-right">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-xs btn-white active">Today</button>
-                        <button type="button" class="btn btn-xs btn-white">Monthly</button>
-                        <button type="button" class="btn btn-xs btn-white">Annual</button>
-                    </div>
-                </div>
+                <h5>PROGRAM</h5>
             </div>
             <div class="ibox-content">
                 <div class="row">
-                    <div class="col-lg-9">
+                    <div class="col-lg-12">
                         <div class="flot-chart">
                             <div class="flot-chart-content" id="flot-dashboard-chart"></div>
                         </div>
                     </div>
-                    <div class="col-lg-3">
+                    {{-- <div class="col-lg-3">
                         <ul class="stat-list">
                             <li>
                                 <h2 class="no-margins">2,346</h2>
@@ -111,7 +104,7 @@
                                 </div>
                             </li>
                         </ul>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
@@ -123,7 +116,7 @@
     <div class="col-lg-12">
         <div class="ibox ">
             <div class="ibox-title">
-                <h5>Custom responsive table </h5>
+                <h5>Agihan Siling Mengikut Program Bagi BP00600 Dan BP01100 </h5>
                 <div class="ibox-tools">
                     <a class="collapse-link">
                         <i class="fa fa-chevron-up"></i>
@@ -161,11 +154,11 @@
                         <tr>
 
                             <th>#</th>
-                            <th>Negeri / Pusat Tanggungjawab </th>
+                            <th>PROGRAM / INSTITUSI / JKN </th>
                             <th>BP00600</th>
                             <th>BP01100</th>
-                            <th>Jumlah </th>
-                            <th>Prestasi</th>
+                            <th>SILING </th>
+                            <th>PRESTASI</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -382,7 +375,7 @@
 
         var dataset = [
             {
-                label: "Number of orders",
+                label: "Jumlah",
                 data: data3,
                 color: "#1ab394",
                 bars: {
@@ -393,7 +386,7 @@
                 }
 
             }, {
-                label: "Payments",
+                label: "Belanja",
                 data: data2,
                 yaxis: 2,
                 color: "#1C84C6",
@@ -421,15 +414,15 @@
 
         var options = {
             xaxis: {
-                mode: "time",
-                tickSize: [3, "day"],
-                tickLength: 0,
-                axisLabel: "Date",
-                axisLabelUseCanvas: true,
-                axisLabelFontSizePixels: 12,
-                axisLabelFontFamily: 'Arial',
-                axisLabelPadding: 10,
-                color: "#d5d5d5"
+                // mode: "time",
+                // tickSize: [3, "day"],
+                // tickLength: 0,
+                // axisLabel: "Date",
+                // axisLabelUseCanvas: true,
+                // axisLabelFontSizePixels: 12,
+                // axisLabelFontFamily: 'Arial',
+                // axisLabelPadding: 10,
+                // color: "#d5d5d5"
             },
             yaxes: [{
                 position: "left",
@@ -466,40 +459,6 @@
         var previousPoint = null, previousLabel = null;
 
         $.plot($("#flot-dashboard-chart"), dataset, options);
-
-        var mapData = {
-            "US": 298,
-            "SA": 200,
-            "DE": 220,
-            "FR": 540,
-            "CN": 120,
-            "AU": 760,
-            "BR": 550,
-            "IN": 200,
-            "GB": 120,
-        };
-
-        $('#world-map').vectorMap({
-            map: 'world_mill_en',
-            backgroundColor: "transparent",
-            regionStyle: {
-                initial: {
-                    fill: '#e4e4e4',
-                    "fill-opacity": 0.9,
-                    stroke: 'none',
-                    "stroke-width": 0,
-                    "stroke-opacity": 0
-                }
-            },
-
-            series: {
-                regions: [{
-                    values: mapData,
-                    scale: ["#1ab394", "#22d6b1"],
-                    normalizeFunction: 'polynomial'
-                }]
-            },
-        });
     });
 </script>
 @endsection
